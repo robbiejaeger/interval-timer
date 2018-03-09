e54bc67ae843a7b86645cf0c0a191b76cf0ecdd3
Robbie Jaeger <jaeger.rob@gmail.com>
2018-03-09 08:40:23 -0700

    Countdown functionality done, but not precise

--- public/index.js
+++ public/index.js
@@ -1,1 +1,27 @@
-console.log('Hello');
+(() => {
+  const interval = 5; // seconds
+  const startButton = document.getElementById('start-timer');
+  const timeDisplay = document.getElementById('time');
+
+  const getTimeRemaining = (endTime) => {
+    const timeRemaining = endTime - (new Date()).getTime();
+    return timeRemaining;
+  };
+
+  const startCountdown = () => {
+    const deadline = (new Date()).getTime() + interval*1000;
+    const intervalID = setInterval(() => {
+      let timeRemaining = getTimeRemaining(deadline);
+      let secondsRemaining = Math.floor( (timeRemaining/1000) % 60 );
+      timeDisplay.innerText = secondsRemaining;
+
+      console.log(timeRemaining)
+      if (timeRemaining <= 0) {
+        clearInterval(intervalID);
+      }
+    }, 100);
+  };
+
+  timeDisplay.innerText = interval;
+  startButton.addEventListener('click', startCountdown);
+})();
